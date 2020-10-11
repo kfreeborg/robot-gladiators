23e46cd7cb5304a153e3e6ccb88ad3b3863ee11f
Kelsey Freeborg <kelsey.freeborg@gmail.com>
2020-10-11 13:55:51 -0700

    final

--- assets/js/solution.js
+++ assets/js/solution.js
@@ -5,7 +5,7 @@ var randomNumber = function (min, max) {
 
 var fightOrSkip = function () {
   // ask user if they'd like to fight or skip using  function
-  var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.', 'FIGHT');
+  var promptFight = window.prompt('Would you like FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.');
 
   // Conditional Recursive Function Call
   if (promptFight === "" || promptFight === null) {
